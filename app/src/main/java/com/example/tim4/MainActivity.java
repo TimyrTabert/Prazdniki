361188f43d758b10aa222fe9edc099a080abcf8b
TimyrTabert <116954563+TimyrTabert@users.noreply.github.com>
2023-01-16 12:58:07 +0600

    Add files via upload

---
+++ app/src/main/java/com/example/tim4/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tim4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout mLinearLayout;
+    private String[] titles = {
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+                intent.putExtra("title", position);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
